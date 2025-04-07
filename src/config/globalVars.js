e115bad5b85e8283b5258777fc1a4c8669635c28
Elias Rodriguez <eliasrod46@gmail.com>
2025-04-07 10:23:21 -0300

    m

--- src/config/globalVars.js
+++ src/config/globalVars.js
@@ -1,7 +1,7 @@
 export const envs = {
   apiServer: {
-    url: "http://127.0.0.1",
-    // url: "http://10.220.10.129",
+    // url: "http://127.0.0.1",
+    url: "http://10.220.10.89",
     port: 8881,
     get serverUrl() {
       return `${this.url}:${this.port}/api`;
